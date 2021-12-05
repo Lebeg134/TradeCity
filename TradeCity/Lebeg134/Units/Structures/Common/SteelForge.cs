873648f939e17807da9ad0021f0166d75de21878
Lebeg134 <cactusmoovies@gmail.com>
2021-12-05 11:47:33 +0100

    Steel forge for testing purposes

--- TradeCity/Lebeg134/Units/Structures/Common/SteelForge.cs
+++ TradeCity/Lebeg134/Units/Structures/Common/SteelForge.cs
@@ -1,6 +1,9 @@
 
 
+using JHP4SD.Lebeg134.Module.Resources;
 using JHP4SD.Lebeg134.Module.Structures;
+using Lebeg134.Resources.Common;
+using System.Collections.Generic;
 /**
 * @(#) SteelForge.cs
 */
@@ -8,7 +11,18 @@ namespace Lebeg134.Structures.Common
 {
 	public class SteelForge: CommonProdBuilding
 	{
-
+		protected static readonly new List<Resource>[] upgradeLevelsCosts = new[]
+		{
+			new List<Resource>{new Wood(100)}
+		};
+		protected static readonly new List<Resource>[] upkeepLevels = new[]
+		{
+			new List<Resource>{new Iron(10)}
+		};
+		protected static readonly new List<Resource>[] productionLevels = new[]
+		{
+			new List<Resource>{new Steel(5)}
+		};
 	}
 	
 }

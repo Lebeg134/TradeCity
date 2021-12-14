53ff8f9db46bcfdb330638603bc65112290fdd3a
Lebeg134 <cactusmoovies@gmail.com>
2021-12-14 17:42:11 +0100

    Added subscribeable visual updater

---
+++ TradeCity/Lebeg134/Module/Graphics/VisualUpdater.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHP4SD.Lebeg134.Module.Graphics
+{
+    class VisualUpdater: IUpdateable
+    {
+        public static VisualUpdater Instance
+        {
+            get
+            {
+                if (_updater == null)
+                    _updater = new VisualUpdater();
+                return _updater;
+            }
+            set
+            {
+                _updater = value;
+            }
+        }
+        static VisualUpdater _updater;
+        List<IUpdateable> subscribers = new List<IUpdateable>();
+
+        public void Register(IUpdateable subscriber)
+        {
+            subscribers.Add(subscriber);
+        }
+        public void UnRegister(IUpdateable subscriber)
+        {
+            if (subscribers.Contains(subscriber))
+                subscribers.Remove(subscriber);
+        }
+        public void Update()
+        {
+            foreach (IUpdateable subscriber in subscribers)
+            {
+                subscriber.Update();
+            }
+        }
+    }
+}

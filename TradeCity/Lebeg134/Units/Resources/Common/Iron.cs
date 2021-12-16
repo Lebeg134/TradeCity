87bf1465785f4688bee58add78a9841322996ea4
Lebeg134 <cactusmoovies@gmail.com>
2021-12-16 11:48:44 +0100

    start on final Unit implementations

--- TradeCity/Lebeg134/Units/Resources/Common/Iron.cs
+++ TradeCity/Lebeg134/Units/Resources/Common/Iron.cs
@@ -14,7 +14,6 @@ namespace Lebeg134.Resources.Common
     public class Iron : StandardResource
     {
         public Iron(int amount) : base(amount) { }
-
         public override SpriteRuntime getSprite()
         {
             return spriteLibrary.IronSprite;
@@ -23,12 +22,10 @@ namespace Lebeg134.Resources.Common
         {
             return "Iron";
         }
-
         public override Resource getNewResource(int amount)
         {
             return new Iron(amount);
         }
-
         public override double getValue()
         {
             return 5;

be218e5bbd847a3cdacb43882eac03a315310293
Lebeg134 <cactusmoovies@gmail.com>
2022-03-28 11:55:34 +0200

    Session done?

--- Assets/Scripts/SessionScript.cs
+++ Assets/Scripts/SessionScript.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class SessionScript : MonoBehaviour
 {
+    Session session;
     // Start is called before the first frame update
     void Start()
     {
-        
+        session = Session.Instance;
     }
 
     // Update is called once per frame

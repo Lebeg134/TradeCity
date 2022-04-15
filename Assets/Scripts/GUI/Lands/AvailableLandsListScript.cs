24ef2c93394658c7cf43d8c51c143945712b1b1e
Lebeg134 <cactusmoovies@gmail.com>
2022-04-15 15:45:15 +0200

    work on generic lists

--- Assets/Scripts/GUI/Lands/AvailableLandsListScript.cs
+++ Assets/Scripts/GUI/Lands/AvailableLandsListScript.cs
@@ -1,38 +1,55 @@
 using JHP4SD.Lebeg134.Module.Session;
 using JHP4SD.Lebeg134.Module.Structures;
 using JHP4SD.Lebeg134.Units.Resources.Common;
+using Lebeg134.Structures.Lands;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AvailableLandsListScript : MonoBehaviour
+public class AvailableLandsListScript : ListScript<Land>
 {
-    public GameObject listItem;
-    public GameObject Content;
-    // Start is called before the first frame update
-    void Start()
+    protected override ICollection<Land> GetCollection()
     {
-        Dictionary<Land, int> offers = Session.Instance.offers;
-        foreach (Land landOffer in offers.Keys)
-        {
-            GameObject landOfferItem = Instantiate(listItem);
-            landOfferItem.GetComponent<LandOfferVisualsScript>().watched = landOffer;
-            landOfferItem.GetComponent<LandOfferVisualsScript>().price = offers[landOffer];
-            landOfferItem.transform.SetParent(Content.transform);
-        }
+        return new List<Land>() { new Forest(), new ClearwaterLake() };
+        //return Session.Instance.offers.Keys;
     }
-    public void Refresh()
+
+    protected override void ProcessListItem(Land item, ref GameObject newListItem)
     {
-        foreach (Transform child in Content.transform)
-        {
-            Destroy(child.gameObject);
-        }
-        Start();
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
+        newListItem.GetComponent<LandOfferVisualsScript>().watched = item;
+        newListItem.GetComponent<LandOfferVisualsScript>().price = 100;
     }
 }
+
+
+//public class AvailableLandsListScript : MonoBehaviour
+//{
+//    public GameObject listItem;
+//    public GameObject Content;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        Dictionary<Land, int> offers = Session.Instance.offers;
+//        foreach (Land landOffer in offers.Keys)
+//        {
+//            GameObject landOfferItem = Instantiate(listItem);
+//            landOfferItem.GetComponent<LandOfferVisualsScript>().watched = landOffer;
+//            landOfferItem.GetComponent<LandOfferVisualsScript>().price = offers[landOffer];
+//            landOfferItem.transform.SetParent(Content.transform);
+//        }
+//    }
+//    public void Refresh()
+//    {
+//        foreach (Transform child in Content.transform)
+//        {
+//            Destroy(child.gameObject);
+//        }
+//        Start();
+//    }
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//    }
+//}
--- Assets/Scripts/GUI/ListScript.cs
+++ Assets/Scripts/GUI/ListScript.cs
@@ -1,21 +1,78 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ListScript : MonoBehaviour
+/// <summary>
+/// Generic List display code for Unity UI elements
+/// </summary>
+/// <typeparam name="T"> Type of the displayed items</typeparam>
+/// <typeparam name="Script"> Script that is present on the ListItem Gameobjects</typeparam>
+/// <typeparam name="Collection"> Type of collection that is to be handled</typeparam>
+public abstract class ListScript<T> : MonoBehaviour
 {
     public GameObject newListItem;
+    public GameObject Content;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Gets called when the script is instantiated
+    /// </summary>
     void Start()
     {
-        
+        Fill();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Fills the Content panel with the items provided in GetCollection()
+    /// List items get processed through ProcessListItem()
+    /// </summary>
+    protected void Fill()
     {
-        
+        foreach (T item in GetCollection())
+        {
+            Instantiate(newListItem);
+            ProcessListItem(item, ref newListItem);
+            newListItem.transform.SetParent(Content.transform);
+        }
     }
+
+    /// <summary>
+    /// Clears the Content panel
+    /// </summary>
+    protected void Clear()
+    {
+        foreach (Transform child in Content.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Clears then refreshes the Content panel
+    /// </summary>
+    public void Refresh()
+    {
+        Clear();
+        Fill();
+    }
+
+    /// <summary>
+    /// Processes List items and sets it's display parameters
+    /// </summary>
+    /// <param name="item"> Item to be displayed</param>
+    /// <param name="newListItem"> Displaying Gameobject that will be added to the list </param>
+    protected abstract void ProcessListItem(T item, ref GameObject newListItem);
+
+    /// <summary>
+    /// Defines the collection to be shown in the list
+    /// </summary>
+    /// <returns> Collection of T </returns>
+    protected abstract ICollection<T> GetCollection();
+
+    /// <summary>
+    /// Gets called once every frame
+    /// Does nothing by default
+    /// </summary>
+    protected void Update() { }
 }

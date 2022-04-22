0f0bfbb896ea295fb3c4d1ce75bd7fdb989093b5
Lebeg134 <cactusmoovies@gmail.com>
2022-04-22 12:41:05 +0200

    started work on model

---
+++ Goal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Goal
+{
+    public bool Check()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public float getProgress()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Mission.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Mission
+{
+    public Goal Goal
+    {
+        get => default;
+        set
+        {
+        }
+    }
+
+    public void ApplyReward()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Check()
+    {
+        throw new System.NotImplementedException();
+    }
+}

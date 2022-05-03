7fb965731003aec41588bb7979b084b99b8a2e5b
Lebeg134 <cactusmoovies@gmail.com>
2022-05-03 15:35:47 +0200

    started work on recipes

---
+++ Assets/Module/Production/Recipe.cs
@@ -0,0 +1,36 @@
+﻿using Lebeg134.Module.Resources;
+using Lebeg134.Module.TimeManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Module.Production
+{
+    internal class Recipe: ITickable
+    {
+        public string Name { get; set; }
+        private List<Resource> input;
+        private List<Resource> output;
+
+        public void AddResource(Resource resource)
+        {
+            
+        }
+        public Resource Produce()
+        {
+
+        }
+
+        public void Tick()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register()
+        {
+            Clock.Instance.Register(this);
+        }
+    }
+}

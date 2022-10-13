a2084875859dd851570ec1cff5f793f52d3ed117
Lebeg134 <cactusmoovies@gmail.com>
2022-10-13 12:19:10 +0200

    Camera zoom V1 implemented

--- TradeCity.Unity/Assets/TradeCity.Unity/Scripts/CameraRig/CameraMovement.cs
+++ TradeCity.Unity/Assets/TradeCity.Unity/Scripts/CameraRig/CameraMovement.cs
@@ -1,52 +1,68 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using AutSoft.UnitySupplements.Vitamins;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace TradeCity.Unity.Scripts.CameraRig
 {
-    [RequireComponent(typeof(Camera))]
     public class CameraMovement : MonoBehaviour
     {
         [SerializeField] private GameObject _focusPointObject = default!;
         [SerializeField] private GameObject _cameraRigGameObject = default!;
+        [SerializeField] private Camera _camera;
+
         [Header("Movement")]
         [SerializeField] private float _maxMovementSpeed = default!;
         [SerializeField] private float _movementAcceleration = default!;
         [SerializeField] private float _turnRate = default!;
         [SerializeField] private float _movementDeAcceleration = default;
+
         [Header("Rotation")]
         [SerializeField] private float _maxRotationSpeed = default!;
         [SerializeField] private float _rotationAcceleration = default!;
         [SerializeField] private float _rotationDeAcceleration = default!;
+
+        [Header("Zoom")]
+        [SerializeField] private float _minHeight = default!;
+        [SerializeField] private float _turnHeight = default!;
+        [SerializeField] private float _basicCameraAngle = default!;
+        [SerializeField] private float _minZoomScale = default!;
+        [SerializeField] private float _maxZoomScale = default!;
+        [SerializeField] private float _zoomSpeed = default!;
         
-
-
-        private Camera _camera;
         private Vector2 _movementVector;
         private float _movementSpeed;
         private float _rotationVelocity;
+        private float _zoomScale = 1f;
 
         private void Awake()
         {
-            _camera = GetComponent<Camera>();
             this.CheckSerializedField(_focusPointObject, nameof(_focusPointObject));
             this.CheckSerializedField(_cameraRigGameObject, nameof(_cameraRigGameObject));
+            this.CheckSerializedField(_camera, nameof(_camera));
         }
 
         private void Update()
         {
             HandleKeys();
+            HandleMouseWheel();
+
 
             _cameraRigGameObject.transform.position += Quaternion.Euler(0, _camera.transform.rotation.eulerAngles.y, 0) *
                                                        new Vector3(_movementVector.x * _movementSpeed * Time.deltaTime,
                                                               0, _movementVector.y * _movementSpeed * Time.deltaTime);
-            RotateAroundMiddle(_rotationVelocity);
+            _cameraRigGameObject.transform.Rotate(Vector3.up, _rotationVelocity);
+        }
+
+        private void HandleMouseWheel()
+        {
+            _zoomScale += -Input.mouseScrollDelta.y*_zoomSpeed;
+            _zoomScale = Mathf.Clamp(_zoomScale, _minZoomScale, _maxZoomScale);
+            MoveCameraPosition();
         }
 
         private void HandleKeys()
         {
-            var offset = _camera.transform.rotation;
-
             var direction = new Vector2();
 
             if (Input.GetKey(KeyCode.W))
@@ -75,7 +91,7 @@ namespace TradeCity.Unity.Scripts.CameraRig
                 }
                 else
                 {
-                    _movementSpeed -= _movementDeAcceleration*Time.deltaTime;
+                    _movementSpeed -= _movementDeAcceleration * Time.deltaTime;
                     if (_movementSpeed < 0)
                     {
                         _movementSpeed = 0;
@@ -90,14 +106,6 @@ namespace TradeCity.Unity.Scripts.CameraRig
 
                 _movementVector = Vector2.MoveTowards(_movementVector, direction.normalized, Time.deltaTime * _turnRate);
             }
-
-            if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && 
-                !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
-            {
-                // nothing TODO remove this if not needed
-            }
-
-            
 
             if (Input.GetKey(KeyCode.Q))
             {
@@ -131,13 +139,21 @@ namespace TradeCity.Unity.Scripts.CameraRig
             _rotationVelocity = _rotationVelocity < -_maxRotationSpeed ? -_maxRotationSpeed : _rotationVelocity;
         }
 
-        private void RotateAroundMiddle(float angle)
-        {
-            var relative = _camera.transform.position - _focusPointObject.transform.position;
-            var rotation = Quaternion.Euler(new Vector3(0, angle, 0));
 
-            _camera.transform.position = rotation * relative + _focusPointObject.transform.position;
-            _camera.transform.eulerAngles = rotation.eulerAngles + _camera.transform.rotation.eulerAngles;
+        private void MoveCameraPosition()
+        {
+            _camera.transform.localPosition = new Vector3(0, Mathf.Sin(_basicCameraAngle)*_zoomScale, -Mathf.Cos(_basicCameraAngle)*_zoomScale);
+
+            var currentHeight = _camera.transform.localPosition.y;
+            var newCameraAngle =  _basicCameraAngle;
+            if (currentHeight < _turnHeight)
+            {
+                var endPhase = (_turnHeight - currentHeight) / (_turnHeight - _minHeight);
+                newCameraAngle -= _basicCameraAngle * endPhase;
+                _camera.transform.localPosition =
+                    new Vector3(0, _camera.transform.localPosition.y + _minHeight * endPhase, _camera.transform.localPosition.z);
+            }
+            _camera.transform.localRotation = Quaternion.Euler(newCameraAngle, 0, 0);
         }
     }
 }

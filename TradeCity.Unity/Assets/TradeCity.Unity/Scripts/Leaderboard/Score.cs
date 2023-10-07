cb2ea096f13bf1882a0ae120fc694b1a3e6f7082
Lebeg134 <cactusmoovies@gmail.com>
2023-10-07 15:43:11 +0200

    Project load and tweaks

---
+++ TradeCity.Unity/Assets/TradeCity.Unity/Scripts/Leaderboard/Score.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace TradeCity.Unity.Scripts.Leaderboard
+{
+    [Serializable]
+    public class Score
+    {
+        private const string Secret = "SomeUniqueIdentifier";
+        public long id;
+        public readonly string username;
+        public readonly long score;
+        public DateTime date;
+
+        public Score(string username, long score)
+        {
+            this.username = username;
+            this.score = score;
+        }
+
+        public int GetSecret()
+        {
+            return Secret.GetHashCode()+username.GetHashCode()+score.GetHashCode();
+        }
+
+    }
+}
---
+++ TradeCity.Unity/Assets/TradeCity.Unity/Scripts/Leaderboard/WebScorePoster.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Text.Json;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace TradeCity.Unity.Scripts.Leaderboard
+{
+    public class WebScorePoster: MonoBehaviour
+    {
+        private const string Url = "http://localhost:8080";
+
+        private Score SendScore(Score score)
+        {
+            WWWForm form = new WWWForm();
+            form.AddField("key", score.GetSecret());
+            string jsonString = JsonSerializer.Serialize(score);
+            using (UnityWebRequest request = UnityWebRequest.PostWwwForm(Url, jsonString))
+            {
+                request.SendWebRequest();
+
+                if (request.error != null)
+                {
+                    //TODO
+                }
+            }
+            return score;
+        }
+    }
+}
